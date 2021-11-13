36c1773d1943a28796a0b7657fc9e3f00338b742
Clerkyrat <55567066+Clerkyrat@users.noreply.github.com>
2021-11-12 19:06:07 -0800

    Update MobLogic.cs
    Chopping Charge state for now to focus on A.I. stability and redoing the movement system.

--- MobLogic.cs
+++ MobLogic.cs
@@ -17,8 +17,6 @@ public class MobLogic : MonoBehaviour
     [Header("Stats")]
     public float moveSpeed;
     private float curSpeed;
-    public int atkSkill;
-    private int atkValue;
     public int endurance;
     private int curEndurance;
 
@@ -27,21 +25,32 @@ public class MobLogic : MonoBehaviour
     public int maxFriends;
     public string foe;
     public string friend;
-    private bool sameName;
+    //private bool sameName;
 
     [Header("Threat and Morale")]
     public int moraleBase;
     private int curMorale;
-    public int threatMin, threatMax;
+    public int thretBase;
     private int curThreat;
-
 
     [Header("Movement")]
     private Vector3 moveDirection;
-    private Vector3 shiftDirection;
-    private Vector3 chargeDirection;
     private Vector3 wanderDirection;
     private Vector3 homePoint;
+
+    [Header("Tools")]
+    public Rigidbody theRB;
+    public Animator anim;
+    public SpriteRenderer theBody;
+    public Transform emotePoint;
+    public GameObject beacon;
+
+    [Header("Emotes")]
+    public GameObject[] emotions;
+    public float timeBetweenEmotes;
+    private float emoteCounter;
+    private bool emoteTick;
+
 
     [Header("Bools")]
     public bool shouldIdle;
@@ -53,6 +62,10 @@ public class MobLogic : MonoBehaviour
     public bool shouldDropItems;
     public bool shouldEmote;
 
+    //Ticks
+    private bool idleTick;
+    private bool wanderTick;
+
 /*-------------------------STATES--------------------------*/
 
     [Header("Ready")]
@@ -60,19 +73,14 @@ public class MobLogic : MonoBehaviour
     private float mfRange;
     public float rangeToChase;
     private bool hasTarget;
-    private bool isGuarding;
-    private float pauseCollider =.3f;
 
     [Header("Idle")]
     public float idleLength;
     private float idleCounter;
-    //public bool blockWhenIdle;
-    //private bool willBlock;
 
     [Header("Wander")]
     public float wanderLength;
     private float wanderCounter;
-    private bool wanderTick;
 
     [Header("Shoot")]
     public GameObject bullet;
@@ -81,23 +89,13 @@ public class MobLogic : MonoBehaviour
     [Header("Melee")]
     public float meleeTimeLength;
     private float meleeCounter;
-    public int attacksPerShift;
-    public int woundThreshold;
-    private bool meleeBump;
-
-    [Header("Shift")]
-    public float shiftTimeLength;
-    private float shiftCounter;
-    private bool shiftTick = false;
 
     [Header("Charge")]
-    public float timeBetweenCharging;
-    private float chargeTime;
-    private bool isCharging;
+    public float chargeLength;
+    private float chargeCounter;
     
     [Header("Gibbing")]
     public bool shouldLeaveCorpse;
-
     public GameObject hitEffect;
     public GameObject[] splatters;
 
@@ -110,21 +108,8 @@ public class MobLogic : MonoBehaviour
     public int curSpriteCount;
     private Sprite curSprite;
 
-    [Header("Tools")]
-    public Rigidbody theRB;
-    //public Animator anim;
-    public SpriteRenderer theBody;
-    public Transform emotePoint;
-    public GameObject beacon;
-
-    [Header("Emotes")]
-    public GameObject[] emotions;
-    public float timeBetweenEmotes;
-    private float emoteCounter;
-    private bool emoteTick;
-
     //Enums
-    enum State {Ready, Idle, Wander, Patrol, Shoot, Melee, Shift, Charge};
+    enum State {Ready, Idle, Wander, Patrol, Shoot, Melee, Charge};
     enum Bump {Friend, Foe, Building}
     State curState = State.Idle;
     State lastState;
@@ -144,10 +129,13 @@ public class MobLogic : MonoBehaviour
         //Timers
         wanderCounter = wanderLength;
         emoteCounter = timeBetweenEmotes;
-        shiftCounter = shiftTimeLength;
         curSpeed = moveSpeed;
         meleeCounter = meleeTimeLength;
         idleCounter = idleLength;
+
+        //Bools
+        if(shouldWander) { wanderTick = true; }
+        if(shouldIdle) { idleTick = true; }
             
         //Time between emote icons and logs apearing
         if (shouldEmote) { emoteTick = true; }
@@ -156,7 +144,7 @@ public class MobLogic : MonoBehaviour
         curSpriteCount = 0;
 
         //Relationships
-        sameName = false;
+        //sameName = false;
 
         //Starting position
         homePoint = transform.position;
@@ -192,28 +180,13 @@ public class MobLogic : MonoBehaviour
         }else{ if(showDebug) { Debug.Log(string.Format("{0} Quiet", namePlate)); } }
     }
 
-    /*public void FriendBump(GameObject other)
+    public void FriendBump()
     {
         if(showDebug) { Debug.Log("Excuse Me"); }
         
         Emote(1);
 
-        //Scan for duplicates in friend list. Needs rework
-        for(int i = 0; i < friendsList.Count; i++)
-        {
-
-            sameName = false;
-
-            if(friendsList[i] == other.gameObject)
-            {
-                sameName = true;
-            }else
-            {
-                //friendsList.Add other.gameObject;
-            }
-            
-        }
-    }*/
+    }
 
     public void Move()
     {
@@ -222,9 +195,11 @@ public class MobLogic : MonoBehaviour
         moveDirection.Normalize();
     }
 
+/*----------------------------------------------------------------------------*/
+
     void FixedUpdate()
     {
-        if(showDebug) { Debug.Log(string.Format("{0}{1}",namePlate, curState)); }
+        //if(showDebug) { Debug.Log(string.Format("{0}{1}",namePlate, curState)); }
 
 /*--------------------------------------------------------*/
 /*-----------------Switches and Logic---------------------*/
@@ -232,116 +207,74 @@ public class MobLogic : MonoBehaviour
         {
 
 
-/*----------------------------------------------------------------------------*/
+    /*----------------------------------------------------------------------------*/
             case State.Ready:
 
-//new plan. Feed every state through Ready.Collider might be causing to many switch calls. Discovered after multiple 
-//emotes spawned while bumping into wall
-
-            //anim.SetBool("IsMoving", false);
-
-            if(meleeBump)
-            {
-                curState = State.Melee;
-            }else
                 {
                     if(lastState == State.Idle)
                     {
                         if(idleCounter <= 0)
                         {
-                            curState = State.Wander;
+                            lastState = State.Wander;
                             idleCounter = Random.Range(idleLength * .75f, idleLength * 1.25f);
-                        }else
-                        {
-                            curState = State.Idle;
                         }
+                    }
+
+                    if(lastState == State.Wander)
+                    {
+
                     }
 
                     if(lastState == State.Melee)
                     {
-                        curState = State.Melee;
-                    }
-
-                    if(lastState == State.Shift)
-                    {
-                        if(shouldCharge)
-                        {
-                            if(hasTarget)
-                            {
-                                mfRange = Vector3.Distance(transform.position, mfToKill.transform.position);
-
-                                if(mfRange > rangeToChase)
-                                {
-                                    if(showDebug) { Debug.Log(string.Format("Lost em {0}", mfRange)); }
-                                    hasTarget = false;
-
-                                    curState = State.Idle;
-                                    //Set to ready and if lacking mfToKill switch to Idle. Ready will be the powerhous of decision making.
-                                }
-
-                                if(mfRange < rangeToChase)
-                                {
-                                    curState = State.Shift;
-                                }
-                            }
-                        }else
-                        {
-                            curState = State.Idle;
-                        }
-
-                    if(lastState ==  State.Charge)
-                    {
-                        curState = State.Charge;
-                    }
-                    
-                    if(lastState == State.Ready)
-                    {
-                        curState = State.Idle;
+                        
                     }
                 }
-            }
 
             lastState = curState;
 
-            break;
-/*--------------------------------------------------------*/
+            if(curState == State.Ready)
+            {
+                curState = State.Idle;
+            }
+
+        break;
+    /*--------------------------------------------------------*/
             case State.Idle:
             
                 if(shouldIdle)
                 {
-                    //anim.SetBool("IsMoving", false);
+                    anim.SetBool("IsMoving", false);
                     if(showDebug) { Debug.Log("I am bored"); }
                     
                     idleCounter -= Time.deltaTime;
 
                     lastState = curState;
                     curState = State.Ready;
+
+
                 }
                 break;
     /*----------------------------------------------------------------------------*/
             case State.Wander:
                 
-                if(shouldWander)
+                if(wanderTick)
                 {
+                    anim.SetBool("IsMoving", true);
+
                     if(showDebug) { Debug.Log("I'm the kind of sprite, who likes to roam around"); }
 
-                    if(!wanderTick)
+                    /*if(!wanderTick)
                     {
                         moveDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
                         wanderTick = true;
-                    }
+                    }*/
 
                     wanderCounter -= Time.deltaTime;
                     
                     Move();
 
-                    if(wanderCounter <= 0)
-                    {
-                        curState = State.Idle;
-                        wanderCounter = Random.Range(wanderLength * .75f, wanderLength * 1.25f);
-                        wanderTick = false;
-                    
-                    }
+                    curState = State.Ready;
                 }
                 break;
     /*----------------------------------------------------------------------------*/
@@ -357,117 +290,21 @@ public class MobLogic : MonoBehaviour
 
                 if(shouldMelee)
                 {
-                    Emote(3);
-                    if(showDebug) { Debug.Log("Prepare to die!"); }
-
-                    atkValue = Random.Range(1, 20) + atkSkill;
-                    if(showDebug) { Debug.Log(string.Format("{0}'s attack roll was {1}", namePlate, atkValue)); }
-
-                    meleeCounter = meleeCounter - Time.deltaTime;
-
-                    if(meleeCounter <= 0)
-                    {
-                        meleeCounter = meleeTimeLength;
-                        curState = State.Ready;
-                    }
-                }else
-                {
-                    curState = State.Ready;
+                    lastState = curState;
                 }
-
                 break;
-
     /*----------------------------------------------------------------------------*/
-            case State.Shift:
-
-                if(!shiftTick)
-                {
-                    shiftTick = true;
-                    shiftDirection = transform.position - mfToKill.transform.position;
-                    moveDirection = shiftDirection;
-                    curSpeed = curSpeed * .75f;
-                }
-
-                Move();
-
-                shiftCounter = shiftCounter - Time.deltaTime;
-
-                if(shiftCounter <= 0)
-                {
-                    shiftTick = false;
-                    curSpeed = moveSpeed;
-                    shiftCounter = shiftTimeLength;
-                    curState = State.Charge;
-                }
-
-                lastState = curState;
-                curState = State.Ready;
-
-            break;
-
-    /*----------------------------------------------------------------------------*/
-    //Bugged, launches off map
-    
             case State.Charge:
 
-            if(shouldCharge)
-            {
-
-                if(mfRange < rangeToChase)
+                if(shouldCharge)
                 {
-
-                    if(!isCharging)
-                    {
-
-
-                        //Direction manipulation
-                        isCharging = true;
-                        homePoint = transform.position;
-                        chargeDirection = new Vector3(mfToKill.transform.position.x, 0f, mfToKill.transform.position.z);
-                        moveDirection = chargeDirection;
-                            
-                        //Debug
-                        if(showDebug)
-                        {
-                            Instantiate(beacon, mfToKill.transform.position, mfToKill.transform.rotation);
-                            Debug.Log(string.Format("CHAAAARGE!"));
-                            Debug.Log(string.Format("They are at {0}", chargeDirection));
-                        }
-                        
-                    }
-
-                    if(isCharging)
-                    {
-                        //chargeDirection = mfToKill.transform.position;
-                        //moveDirection = chargeDirection;
-                        Move();
-                        chargeTime = chargeTime + Time.deltaTime;
-                    }
-
-                    //Debug t stop void running
-                    if(chargeTime >= 10f)
-                    {
-                        curState = State.Idle;
-                        hasTarget = false;
-                        transform.position = homePoint;
-                        if(showDebug) { Debug.Log(string.Format("Let the man in Black go, Gunslinger")); }
-                        chargeTime = 0f;
-                        isCharging = false;
-                    }
-
-                }else
-                {
-                    isCharging = false;
-                    curState = State.Ready;
+                    lastState = curState;
                 }
-                
-                lastState = curState;
-                curState = State.Ready;
-            }
+            
             break;
 
-    /*----------------------------------------------------------------------------*/
-    /*--------------------------------Timers--------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/*--------------------------------Timers--------------------------------------*/
         }
 
         //Emote
@@ -480,96 +317,60 @@ public class MobLogic : MonoBehaviour
             emoteCounter = timeBetweenEmotes;
         }
 
-        //Collider
-        pauseCollider = pauseCollider - Time.deltaTime;
-        
-        
-/*----------------------------------------------------------------------------*/
-/*--------------------------Sprites and GFX-----------------------------------*/
-
-//        curSprite = bodies[curSpriteCount];
-//        theBody.sprite = curSprite;
     }
 
-    /*----------------------------------------------------------------------------*/
-    /*-----------------------------Collisions-------------------------------------*/
+
+/*-----------------------------Collisions-------------------------------------*/
 
     private void OnCollisionEnter(Collision other)
     {
-        if(pauseCollider <= 0f)
+        if(showDebug) { Debug.Log(string.Format("Collide" )); }
+
+        //Friendly
+        if(other.gameObject.tag == friend)
         {
-            pauseCollider = .3f;
-
-            if(showDebug) { Debug.Log(string.Format("Collide" )); }
-
-            //Possibly convert the following into a Switch statement as it expands
-            
-            //switch:
-
-            //Friendly
-            if(other.gameObject.tag == friend)
-            {
-                //FriendBump(other.gameObject);
-            }
-
-            //Enemy
-            if(other.gameObject.tag == foe)
-            {
-
-                if(!hasTarget && shouldMelee)
-                {
-                    meleeBump = true;
-                    mfToKill = other.gameObject;
-                    hasTarget =true;
-                    //curState = State.Melee;
-
-                    if(showDebug) { Debug.Log(string.Format("Kill that SOB {0}", mfToKill)); }
-                }
-
-                if(other.gameObject == mfToKill && hasTarget)
-                {
-                    isCharging = false;
-                    //curState = State.Melee;
-                }
-            }
-
-            //Bump
-            if(other.gameObject.tag == "Building")
-            {
-                if(!isCharging)
-                {
-                    lastState = curState;
-                    if(showDebug) { Debug.Log("Ooof"); }
-                    //wanderCounter = wanderCounter + 1f;
-                    moveDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
-                }
-            }
         }
+
+        //Enemy
+        if(other.gameObject.tag == foe)
+        {
+            mfToKill = other.gameObject;
+
+            if(showDebug) { Debug.Log(string.Format("Kill that SOB {0}", mfToKill)); }
+        }
+
+        //Bump
+        if(other.gameObject.tag == "Building")
+        {
+                moveDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
+        }
+        
     }
     
     private void OnCollisionStay(Collision other) 
     {
         if(other.gameObject.tag == "Building")
             {
-                if(!isCharging)
-                {
-                    //wanderCounter = wanderCounter + 1f;
-                    moveDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
-                }
-                
+                moveDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
             }
+                
     }
+/*----------------------------------------------------------------------------*/
+/*--------------------------Sprites and GFX-----------------------------------*/
+
+        //curSprite = bodies[curSpriteCount];
+        //theBody.sprite = curSprite;
+}
 
 
     /* CODE SNIPPETS
 
     if(showDebug) { Debug.Log(string.Format("")); }
 
-    //Initial atk or volley 
-    //compare ATk roll to target. Holding value of atk for a second i case of multiple attackers (possible opponent counter down the line to change animations)
-    //timer between attacks in SHift state
-    //Charge towards mftoKills
-    //revert back to melee state
-
-    */
-}
+    //Combat
+    /*Initial atk or volley 
+    compare ATk roll to target. Holding value of atk for a second i case of multiple attackers
+    (possible opponent counter down the line to change animations)
+    timer between attacks in SHift state
+    Charge towards mftoKills
+    revert back to melee state*/
